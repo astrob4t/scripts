2f52cec9de94added1784298e6803d52fdb986df
astrob4t <abelfromlyle@gmail.com>
2024-11-27 19:24:02 -0800

    Edit the coffeeMachine

--- pyscripts/coffeeMachine.py
+++ pyscripts/coffeeMachine.py
@@ -17,14 +17,15 @@ type = input ("What type of coffee are you making? ")
 # Determine if you want coffee
 coffee = 0
 r = 1
-while r <= 7:
-  coffee += random.randint(1,2)
+
+while r <= 10:
+  coffee += random.randint(0,10)
   print("Your coffee value as of this message is",coffee)
   time.sleep (0.225)
   r += 1
 
-# Tell the user if they get coffee
-if coffee >= 10:
+  # Tell the user if they get coffee
+if coffee < 50:
     print('Yes, go get your', type)
     print(coffee)
 else:
